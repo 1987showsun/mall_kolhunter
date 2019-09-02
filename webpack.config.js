d9213ad0e654eb76aacaaeab04f3555ede382d80
黎育存 <94412108@stud.sju.edu.tw>
2019-09-02 11:29:13 +0800

    [store] webpack set

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,8 @@ Object.keys(process.env).map((key, i) => {
 });
 if( keyName['NODE_ENV']=='development' ){
   //SETUP.push({'NODE_ENV_DEV': true})
-  //SETUP['NODE_ENV_DEV'] = true;
-  SETUP = { ...SETUP, 'NODE_ENV_DEV': true }
+  SETUP['NODE_ENV_DEV'] = true;
+  //SETUP = { ...SETUP, 'NODE_ENV_DEV': true }
 }
 
 const browserConfig = {

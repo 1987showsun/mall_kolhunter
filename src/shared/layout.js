ec4b55c7aa957ca1be2ec48da39e26f17507cf9e
黎育存 <94412108@stud.sju.edu.tw>
2019-10-30 14:52:00 +0800

    [store] add chat

--- src/shared/layout.js
+++ src/shared/layout.js
@@ -1,15 +1,17 @@
-import $ from 'jquery';
-import React from 'react';
-import queryString from 'query-string';
-import { connect } from 'react-redux';
-import { Route, Switch, Redirect } from "react-router-dom";
+import $                           from 'jquery';
+import React                       from 'react';
+import queryString                 from 'query-string';
+import { connect }                 from 'react-redux';
+import { Route, Switch, Redirect, Link } from "react-router-dom";
+import { FontAwesomeIcon }               from '@fortawesome/react-fontawesome';
+import { faQuestion }                    from '@fortawesome/free-solid-svg-icons';
 
 //Routes
-import routers from './routers';
+import routers                     from './routers';
 
 //Components
-import Header from './components/common/header/header';
-import Footer from './components/common/footer/footer';
+import Header                      from './components/common/header/header';
+import Footer                      from './components/common/footer/footer';
 
 //Stylesheets
 import './public/stylesheets/common.scss';
@@ -61,19 +63,25 @@ class Layout extends React.Component{
                             location= {location}
                         />
                 }
+                {/* <div className="all-help">
+                    <Link to="/vendor/leading">
+                        <FontAwesomeIcon icon={faQuestion} />
+                        <span className="text">想上架</span>
+                    </Link>
+                </div> */}
             </React.Fragment>
         );
     }
 
     componentDidUpdate(prevProps, prevState) {
-        const { location, match } = this.props;
-        const pathname = location.pathname.split('/').filter( item => item!='' );
-        const search   = queryString.parse(location.search);
+        const { location }      = this.props;
+        const pathname          = location.pathname.split('/').filter( item => item!='' );
+        const search            = queryString.parse(location.search);
         const prevPropsLocation = prevProps.location;
-        const prevPathname = prevPropsLocation.pathname.split('/').filter( item => item!='' );
-        const prevSearch = queryString.parse(prevPropsLocation.search);
-        let pathnameComparison = false;
-        let searchComparison   = false;
+        const prevPathname      = prevPropsLocation.pathname.split('/').filter( item => item!='' );
+        const prevSearch        = queryString.parse(prevPropsLocation.search);
+        let pathnameComparison  = false;
+        let searchComparison    = false;
 
         if( pathname.length>prevPathname.length ){
             pathnameComparison = pathname.some( (keys,i) => keys!=prevPathname[i] );

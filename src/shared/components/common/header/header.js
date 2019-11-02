59bcf3193bab8c4aa0c854e82755a8bd2e326dac
黎育存 <94412108@stud.sju.edu.tw>
2019-11-03 03:25:10 +0800

    [store] 1. 新增 header 廠商登入連結 2. 新增 fb ig icon 3. 修改商品購買頁style

--- src/shared/components/common/header/header.js
+++ src/shared/components/common/header/header.js
@@ -1,155 +1,169 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { FontAwesomeIcon }from '@fortawesome/react-fontawesome';
+import React                  from 'react';
+import { Link }               from 'react-router-dom';
+import { connect }            from 'react-redux';
+import { FontAwesomeIcon }    from '@fortawesome/react-fontawesome';
 import { faUser, faStore, faUserPlus, faShoppingCart, faTruck }from '@fortawesome/free-solid-svg-icons';
 
 //Compoents
-import Search from './search';
+import Search                 from './search';
+import Top                    from './top';
 
 // Actions
-import { ainfo, cartsCount } from '../../../actions/myaccount';
-import { getCartID } from '../../../actions/common';
+import { ainfo, cartsCount }  from '../../../actions/myaccount';
+import { getCartID }          from '../../../actions/common';
 
 // stylesheets
 import './header.scss';
 
 // Images
-import Logo from '../../../public/images/logo.png';
-import LogoMobile from '../../../public/images/logo-mobile.png';
+import Logo                   from '../../../public/images/logo.png';
+import LogoMobile             from '../../../public/images/logo-mobile.png';
 
 class Header extends React.Component{
 
     constructor(props){
         super(props);
         this.state = {
-            token: props.token,
-            accountInfo: props.accountInfo,
-            cartsCount: props.cartsCount
+            accountToken: props.accountToken,
+            vendorToken : props.vendorToken,
+            accountInfo : {},
+            cartsCount  : 0
         }
     }
 
     static getDerivedStateFromProps(props, state){
 
-        if( props.token!=state.token ){
-            return{
-                cartsCount: props.cartsCount,
-                token: props.token
-            }
+        let cartsCount    = state.cartsCount;
+        let accountInfo   = state.accountInfo;
+        let accountToken  = state.accountToken;
+        let vendorToken   = state.vendorToken;
+
+        if( props.cartsCount!=cartsCount ){
+            cartsCount  = props.cartsCount;
+        }
+        if( props.vendorToken!=vendorToken ){
+            vendorToken = props.vendorToken;
+        }
+        if( props.accountInfo!=accountInfo ){
+            accountInfo = props.accountInfo;
+        }
+        if( props.accountToken!=accountToken ){
+            accountToken = props.accountToken;
         }
 
-        if( props.accountInfo!=state.accountInfo ){
-            return{
-                cartsCount: props.cartsCount,
-                token: props.token,
-                accountInfo: props.accountInfo
-            }
-        }
         return {
-            cartsCount: props.cartsCount
+            accountToken  : accountToken,
+            vendorToken   : vendorToken,
+            accountInfo   : accountInfo,
+            cartsCount    : cartsCount
         };
     }
 
     render(){
 
-        const { accountInfo, token, cartsCount } = this.state;
+        const { accountInfo, accountToken, vendorToken, cartsCount } = this.state;
         const { history, match, location } = this.props;
         const pathname = location['pathname'].split('/').filter( item => item!='' );
-        const cartPathUrl   = token!="" && token!=undefined? '/myaccount/carts' :'/account?to=carts';
-        const ordersPathUrl = token!="" && token!=undefined? '/myaccount/orders':'/account?to=orders';
+        const cartPathUrl   = accountToken!="" && accountToken!=undefined? '/myaccount/carts' :'/account?to=carts';
+        const ordersPathUrl = accountToken!="" && accountToken!=undefined? '/myaccount/orders':'/account?to=orders';
 
         return(
             <header data-content="center">
                 <section className="container">
-                    <div className="logo-block">
-                        <Link to="/">
-                            <img className="normal" src={Logo} alt="" title="" />
-                            <img className="mobile" src={LogoMobile} alt="" title="" />
-                        </Link>
-                    </div>
-                    <Search 
-                        history= {history}
-                        match= {match}
-                        location= {location}
+                    <Top 
+                        vendorToken= {vendorToken}
                     />
-                    <div className="header-nav-block">
-                        <ul>
-                            {
-                                token!="" && token!=null && token!=undefined ? (
-                                    <React.Fragment>
-                                        <li className={``}>
-                                            <Link to="/myaccount">
-                                                {
-                                                    accountInfo['photo']!=null?(
-                                                        <span className="icon-block">
-                                                            <img src={accountInfo['photo']} alt="" title="" />
-                                                        </span>
-                                                    ):(
-                                                        <span className="icon-block">
-                                                            <FontAwesomeIcon icon={faUser} />
-                                                        </span>
-                                                    )
-                                                }
-                                                <div className="prompt-block">{accountInfo['name']}</div>
-                                            </Link>
-                                        </li>
-                                        {
-                                            accountInfo['celebrity']==1 &&
-                                                <li className={`${pathname[0]=='mystore'}`}>
-                                                    <Link to="/mystore">
-                                                        <span className="icon-block">
-                                                            <FontAwesomeIcon icon={faStore} />
-                                                        </span>
-                                                        <div className="prompt-block">店舖管理</div>
-                                                    </Link>
-                                                </li>
-                                        }
-                                    </React.Fragment>
-                                ):(
-                                    <React.Fragment>
-                                        <li>
-                                            <Link to="/account?back=true">
-                                                <span className="icon-block">
-                                                    <FontAwesomeIcon icon={faUser} />
-                                                </span>
-                                                <div className="prompt-block">會員登入</div>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link to="/account/signup">
-                                                <span className="icon-block">
-                                                    <FontAwesomeIcon icon={faUserPlus} />
-                                                </span>
-                                                <div className="prompt-block">加入會員</div>
-                                            </Link>
-                                        </li>
-                                    </React.Fragment>
-                                )
-                            }
-                            <li className={`${pathname[1]=='carts'}`}>
-                                <Link to={cartPathUrl}>
-                                    <span className="icon-block">
-                                        <FontAwesomeIcon icon={faShoppingCart} />
-                                        {
-                                            // 購物車商品為0時不顯示
-                                            cartsCount!=0 &&
-                                                <span className="icon-block-number">{cartsCount}</span>
-                                        }
-                                    </span>
-                                    <div className="prompt-block">
-                                        購物車
-                                    </div>
-                                </Link>
-                            </li>
-                            <li className={`${pathname[1]=='orders'}`}>
-                                <Link to={ordersPathUrl}>
-                                    <span className="icon-block">
-                                        <FontAwesomeIcon icon={faTruck} />
-                                    </span>
-                                    <div className="prompt-block">我的訂單</div>
-                                </Link>
-                            </li>
-                        </ul>
+                    <div className="bottom">
+                        <div className="logo-block">
+                            <Link to="/">
+                                <img className="normal" src={Logo} alt="" title="" />
+                                <img className="mobile" src={LogoMobile} alt="" title="" />
+                            </Link>
+                        </div>
+                        <Search 
+                            match     = {match}
+                            history   = {history}
+                            location  = {location}
+                        />
+                        <div className="header-nav-block">
+                            <ul>
+                                {
+                                    accountToken!="" && accountToken!=null && accountToken!=undefined ? (
+                                        <React.Fragment>
+                                            <li>
+                                                <Link to="/myaccount">
+                                                    {
+                                                        accountInfo['photo']!=null?(
+                                                            <span className="icon-block accountPhoto">
+                                                                <img src={accountInfo['photo']} alt={accountInfo['name']} title={accountInfo['name']} />
+                                                            </span>
+                                                        ):(
+                                                            <span className="icon-block">
+                                                                <FontAwesomeIcon icon={faUser} />
+                                                            </span>
+                                                        )
+                                                    }
+                                                    <div className="prompt-block">{accountInfo['name']}</div>
+                                                </Link>
+                                            </li>
+                                            {
+                                                accountInfo['celebrity']==1 &&
+                                                    <li className={`${pathname[0]=='mystore'}`}>
+                                                        <Link to="/mystore">
+                                                            <span className="icon-block">
+                                                                <FontAwesomeIcon icon={faStore} />
+                                                            </span>
+                                                            <div className="prompt-block">店舖管理</div>
+                                                        </Link>
+                                                    </li>
+                                            }
+                                        </React.Fragment>
+                                    ):(
+                                        <React.Fragment>
+                                            <li>
+                                                <Link to="/account?back=true">
+                                                    <span className="icon-block">
+                                                        <FontAwesomeIcon icon={faUser} />
+                                                    </span>
+                                                    <div className="prompt-block">會員登入</div>
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/account/signup">
+                                                    <span className="icon-block">
+                                                        <FontAwesomeIcon icon={faUserPlus} />
+                                                    </span>
+                                                    <div className="prompt-block">加入會員</div>
+                                                </Link>
+                                            </li>
+                                        </React.Fragment>
+                                    )
+                                }
+                                <li className={`${pathname[1]=='carts'}`}>
+                                    <Link to={cartPathUrl}>
+                                        <span className="icon-block">
+                                            <FontAwesomeIcon icon={faShoppingCart} />
+                                            {
+                                                // 購物車商品為0時不顯示
+                                                cartsCount!=0 &&
+                                                    <span className="icon-block-number">{cartsCount}</span>
+                                            }
+                                        </span>
+                                        <div className="prompt-block">
+                                            購物車
+                                        </div>
+                                    </Link>
+                                </li>
+                                <li className={`${pathname[1]=='orders'}`}>
+                                    <Link to={ordersPathUrl}>
+                                        <span className="icon-block">
+                                            <FontAwesomeIcon icon={faTruck} />
+                                        </span>
+                                        <div className="prompt-block">我的訂單</div>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </section>
             </header>
@@ -170,9 +184,10 @@ class Header extends React.Component{
 
 const mapStateToProps = state => {
     return{
-        token: state.login.jwt_account,
-        accountInfo: state.myaccount.info,
-        cartsCount: state.myaccount.cartsCount
+        vendorToken  : state.login.jwt_vendor,
+        accountToken : state.login.jwt_account,
+        accountInfo  : state.myaccount.info,
+        cartsCount   : state.myaccount.cartsCount
     }
 }
 
---
+++ src/shared/components/common/header/top.js
@@ -0,0 +1,52 @@
+import React                  from 'react';
+import { Link }               from 'react-router-dom';
+import { FontAwesomeIcon }    from '@fortawesome/react-fontawesome';
+import { faInstagram }from '@fortawesome/free-solid-svg-icons';
+
+// Images
+import instagram from '../../../public/images/icon/instagram.svg';
+import facebook  from '../../../public/images/icon/facebook.svg';
+
+
+export default ( props ) => {
+
+    const { vendorToken } = props;
+
+    return(
+        <div className="top">
+            <ul>
+                {
+                    vendorToken!="" && vendorToken!=null && vendorToken!=undefined ? (
+                        <>
+                            <li>
+                                <Link to="/myvendor">廠商管理系統</Link>
+                            </li>
+                        </>
+                    ):(
+                        <>
+                            <li>
+                                <Link to="/vendor">廠商登入</Link>
+                            </li>
+                            <li>
+                                <Link to="/vendor/signup">加入經銷商</Link>
+                            </li>
+                        </>
+                    )
+                }
+                <li>
+                    <span>追蹤我們</span>
+                    <span className="share-icon">
+                        <a to="https://www.facebook.com/%E7%B6%B2%E7%B4%85%E9%9B%BB%E5%95%86-KOL-Mall-106307374129745/" target="_blank">
+                            <img src={facebook} alt="網紅電商 facebook" title="網紅電商 facebook"/>
+                        </a>
+                    </span>
+                    <span className="share-icon">
+                        <a to="https://www.instagram.com/explore/locations/106307374129745/kol-mall/" target="_blank">
+                            <img src={instagram} alt="網紅電商 instagram" title="網紅電商 instagram"/>
+                        </a>
+                    </span>
+                </li>
+            </ul>
+        </div>
+    );
+}

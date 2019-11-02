f05d8bc15c388d4e9305db0115db207897799a7c
黎育存 <94412108@stud.sju.edu.tw>
2019-11-02 17:37:03 +0800

    [store] 1.新增icon 2. 新增幫助浮動區塊 3. 新增幫助浮動style

--- src/shared/layout.js
+++ src/shared/layout.js
@@ -15,6 +15,7 @@ import Footer                            from './components/common/footer/footer
 
 // Images
 import line                              from './public/images/icon/icons8-line-144.png';
+import service                           from './public/images/icon/service.png';
 
 //Stylesheets
 import './public/stylesheets/common.scss';
@@ -67,10 +68,12 @@ class Layout extends React.Component{
                         />
                 }
                 <div className="permanent-tool">
+                    <div className="permanent-tool-head">幫助</div>
                     <ul className="permanent-tool-list">
                         <li>
                             <a href="http://nav.cx/AalEgTs" target="_blank">
-                                <img src={line} alt="Mall Kolhunter line" title="Mall Kolhunter line" />
+                                <img src={service} alt="Mall Kolhunter line" title="Mall Kolhunter line" />
+                                <span className="permanent-tool-prompt">我要上架</span>
                             </a>
                         </li>
                     </ul>

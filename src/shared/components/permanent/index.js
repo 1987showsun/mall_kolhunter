c0075bd97e486590ccfb5f55117bafac1991159b
黎育存 <94412108@stud.sju.edu.tw>
2019-11-05 13:05:27 +0800

    [store] 1. 右下角常駐幫助 樣式修改

--- src/shared/components/permanent/index.js
+++ src/shared/components/permanent/index.js
@@ -1,13 +1,14 @@
 import React                             from 'react';
 
 // Images
-import line                              from '../../public/images/icon/icons8-line-144.png';
 import service                           from '../../public/images/icon/service.png';
 
 export default () => {
     return(
         <div className="permanent-tool">
-            <div className="permanent-tool-head">幫助</div>
+            <label htmlFor="permanent-switch" className="permanent-tool-head">幫助</label>
+            <input type="checkbox" id="permanent-switch" name="permanent-switch"/>
+            <label htmlFor="permanent-switch" className="permanent-tool-mask"></label>
             <ul className="permanent-tool-list">
                 <li>
                     <a href="http://nav.cx/AalEgTs" target="_blank">

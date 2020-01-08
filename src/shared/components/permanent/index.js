9dd9637bd0f65a71c4f9ba6b08153018c15a5f43
Logi Khoo <logi@dpc.tw>
2020-01-08 18:47:18 +0800

    [misc] 加客服icon + line連結

--- src/shared/components/permanent/index.js
+++ src/shared/components/permanent/index.js
@@ -13,6 +13,7 @@ import './public/stylesheets/style.scss';
 // Images
 import service                           from '../../public/images/icon/service.png';
 import kol                               from '../../public/images/icon/kol.png';
+import support                           from '../../public/images/icon/support.png';
 
 export default () => {
     return(
@@ -28,6 +29,12 @@ export default () => {
                     <a href="http://nav.cx/AalEgTs" target="_blank">
                         <img src={service} alt="Mall Kolhunter line" title="Mall Kolhunter line" />
                         <span className="permanent-tool-prompt">我要上架</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://line.me/R/ti/p/%40936zplfo" target="_blank">
+                        <img src={support} alt="Mall Kolhunter support" title="Mall Kolhunter support" />
+                        <span className="permanent-tool-prompt">客服</span>
                     </a>
                 </li>
                 <li>

326ddec3a6ddfbf440b335ffe16054372bf2a73b
Logi Khoo <logi@dpc.tw>
2020-02-12 11:34:28 +0800

    [store] 組合產品細節頁-如果全部的規格只有一個，不要顯示規格區塊

--- src/shared/pages/product/detail/components/cover/spec.js
+++ src/shared/pages/product/detail/components/cover/spec.js
@@ -13,56 +13,68 @@ export default (props) => {
     const { formObject, data } = props;
     const { spec, combo }      = data;
 
-    return(
-        <>
-            {
-                Object.keys( spec ).map((keys, k_i) => {
-                    return(
-                        <div key={keys} className="detail-cover-row cover-select">
-                            <label>
-                                {
-                                    combo? (
-                                        `${lang['zh-TW']['label']['combo']} ${k_i+1}`
-                                    ):(
-                                        lang['zh-TW']['label']['size']
-                                    )
-                                }
-                            </label>
-                            <ul className="select-list">
-                                {
-                                    spec[keys].map( (item,i) => {
-                                        return(
-                                            <li key={item['token']}>
-                                                <label htmlFor={`${k_i}_${item['token']}`}>
-                                                    <input 
-                                                        type          = "radio"
-                                                        className     = "variant"
-                                                        name          = {`${k_i}`}
-                                                        id            = {`${k_i}_${item['token']}`}
-                                                        value         = {item['token']}
-                                                        onChange      = { e => {
-                                                            const { formObject }  = props;
-                                                            const { name, value } = e.target;
-                                                            let   { specToken }   = formObject;
-                                                            specToken[name] = value;
-                                                            props.returnSpecToken({...formObject,specToken});
-                                                        }}
-                                                        checked       = { formObject['specToken'][k_i]==item['token'] }
-                                                        disabled      = { item.hasOwnProperty('storage')? item['storage']==0 || false : false }
-                                                    />
-                                                    <span>
-                                                        {item['name']} 
-                                                    </span>
-                                                </label>
-                                            </li>
-                                        );
-                                    })
-                                }
-                            </ul>
-                        </div>
-                    );
-                })
-            }
-        </>
-    );
+    let showSpecs = false
+    Object.keys( spec ).map((keys, k_i) => {
+        if (spec[keys].length > 1) {
+            showSpecs = true;
+        }
+    })
+
+    if (combo==true && showSpecs==false) {
+        return null;
+    } else {
+        return(
+            <>
+                {
+                    Object.keys( spec ).map((keys, k_i) => {
+                        return(
+                            <div key={keys} className="detail-cover-row cover-select">
+                                <label>
+                                    {
+                                        combo? (
+                                            `${lang['zh-TW']['label']['combo']} ${k_i+1}`
+                                        ):(
+                                            lang['zh-TW']['label']['size']
+                                        )
+                                    }
+                                </label>
+                                <ul className="select-list">
+                                    {
+                                        spec[keys].map( (item,i) => {
+                                            return(
+                                                <li key={item['token']}>
+                                                    <label htmlFor={`${k_i}_${item['token']}`}>
+                                                        <input 
+                                                            type          = "radio"
+                                                            className     = "variant"
+                                                            name          = {`${k_i}`}
+                                                            id            = {`${k_i}_${item['token']}`}
+                                                            value         = {item['token']}
+                                                            onChange      = { e => {
+                                                                const { formObject }  = props;
+                                                                const { name, value } = e.target;
+                                                                let   { specToken }   = formObject;
+                                                                specToken[name] = value;
+                                                                props.returnSpecToken({...formObject,specToken});
+                                                            }}
+                                                            checked       = { formObject['specToken'][k_i]==item['token'] }
+                                                            disabled      = { item.hasOwnProperty('storage')? item['storage']==0 || false : false }
+                                                        />
+                                                        <span>
+                                                            {item['name']} 
+                                                        </span>
+                                                    </label>
+                                                </li>
+                                            );
+                                        })
+                                    }
+                                </ul>
+                            </div>
+                        );
+                    })
+                }
+            </>
+        );
+    }
+    
 }

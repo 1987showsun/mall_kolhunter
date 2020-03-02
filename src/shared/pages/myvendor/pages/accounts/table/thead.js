7dbaa25a121a80623f0a0125e3da476d6c3370c5
Logi Khoo <logi@dpc.tw>
2020-03-02 12:12:27 +0800

    Revert "[misc] vendor report equation styling"
    This reverts commit 61e0e9600e73e5f5a2628c16adac60e0569b537e.

--- src/shared/pages/myvendor/pages/accounts/table/thead.js
+++ src/shared/pages/myvendor/pages/accounts/table/thead.js
@@ -12,9 +12,7 @@ export default () => {
             <div>訂購人</div>
             {/* <div>訂單狀態</div> */}
             <div className="text-right">分潤後訂單總額</div>
-            <div className="text-right mod"></div>
             <div className="text-right">手續費</div>
-            <div className="text-right mod"></div>
             <div className="text-right">應收款項</div>
             <div>訂購時間</div>
         </>

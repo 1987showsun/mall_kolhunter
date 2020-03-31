dbfff37170102b8e903e6effd6067b31d67c678e
Logi Khoo <logi@dpc.tw>
2020-03-31 16:32:30 +0800

    [misc] store fan orders - display refunded items strikethrough

--- src/shared/pages/mystore/pages/fansorders/components/table/tbody.js
+++ src/shared/pages/mystore/pages/fansorders/components/table/tbody.js
@@ -6,24 +6,24 @@
 import React          from 'react';
 import CurrencyFormat from 'react-currency-format';
 
-export default ({orderID, productName, count, purchasePrice, totalPrice, kolFeeSplit, productFee}) => {
+export default ({orderID, productName, count, purchasePrice, totalPrice, kolFeeSplit, productFee, refundStatus}) => {
     return(
         <>
-<           div className="table-new-cell table-new-head">{orderID}</div>
+            <div className="table-new-cell table-new-head">{orderID}{refundStatus!='none' ? ' (已退貨)' : ''}</div>
             <div className="table-new-cell table-new-head">{productName}</div>
             <div className="table-new-cell table-new-head text-right">
                 <CurrencyFormat value={count} displayType={'text'} thousandSeparator={true} />
             </div>
-            <div className="table-new-cell table-new-head text-right money">
+            <div className={`table-new-cell table-new-head text-right money` + (refundStatus!='none' ? ' refunded' : '')}>
                 <CurrencyFormat value={purchasePrice} displayType={'text'} thousandSeparator={true} prefix={'$'} />
             </div>
-            <div className="table-new-cell table-new-head text-right money">
+            <div className={`table-new-cell table-new-head text-right money` + (refundStatus!='none' ? ' refunded' : '')}>
                 <CurrencyFormat value={totalPrice} displayType={'text'} thousandSeparator={true} prefix={'$'} />
             </div>
-            <div className="table-new-cell table-new-head text-right">
+            <div className={`table-new-cell table-new-head text-right money` + (refundStatus!='none' ? ' refunded' : '')}>
                 <CurrencyFormat value={kolFeeSplit*100} displayType={'text'} thousandSeparator={true} /> ％
             </div>
-            <div className="table-new-cell table-new-head text-right money">
+            <div className={`table-new-cell table-new-head text-right money` + (refundStatus!='none' ? ' refunded' : '')}>
                 <CurrencyFormat value={productFee} displayType={'text'} thousandSeparator={true} prefix={'$'} />
             </div>
         </>

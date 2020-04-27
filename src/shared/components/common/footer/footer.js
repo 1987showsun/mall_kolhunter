66f5923c34166f8d5e9b64588edfd6c0dfdbe35e
Logi Khoo <logi@dpc.tw>
2020-04-27 17:27:45 +0800

    [misc] footer link - contact us

--- src/shared/components/common/footer/footer.js
+++ src/shared/components/common/footer/footer.js
@@ -98,7 +98,7 @@ class Footer extends React.Component{
                             <div className="footer-nav-wrap" data-direction="column">
                                 <div className="footer-nav-wrap-title">其他服務</div>
                                 <ul>
-                                    <li><Link to="">聯絡我們</Link></li>
+                                    <li><Link to="/terms/contact">聯絡我們</Link></li>
                                     <li><Link to="/terms/refund">退貨政策</Link></li>
                                 </ul>
                             </div>
